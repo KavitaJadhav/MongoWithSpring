ec87fc3c6632264d2771f6a5518d2e950d68a63c
Kavita Jadhav <jadhavkavita206@gmail.com>
2021-10-06 22:02:22 +0530

    Initial commit.

---
+++ src/main/java/com/example/MongoWithSpring/MongoWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.MongoWithSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoWithSpringApplication.class, args);
+	}
+
+}
