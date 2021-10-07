992f811834863276042bdef4ea4baab0e65e6d08
Kavita Jadhav <jadhavkavita206@gmail.com>
2021-10-07 10:35:13 +0530

    Added student repository.

--- src/main/java/com/example/MongoWithSpring/MongoWithSpringApplication.java
+++ src/main/java/com/example/MongoWithSpring/MongoWithSpringApplication.java
@@ -2,12 +2,14 @@ package com.example.MongoWithSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories("com.example.repository")
 @SpringBootApplication
 public class MongoWithSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MongoWithSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MongoWithSpringApplication.class, args);
+    }
 
 }
