1768a5592959f0d8de1498af201b9c063b056910
Kavita Jadhav <jadhavkavita206@gmail.com>
2021-10-07 19:17:06 +0530

    Added endpoint ton create student.

--- src/main/java/com/example/MongoWithSpring/MongoWithSpringApplication.java
+++ src/main/java/com/example/MongoWithSpring/MongoWithSpringApplication.java
@@ -2,10 +2,12 @@ package com.example.MongoWithSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongoRepositories("com.example.repository")
 @SpringBootApplication
+@ComponentScan("com.example.*")
 public class MongoWithSpringApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/controller/StudentController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import com.example.entity.Student;
+import com.example.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+    @Autowired
+    StudentService studentService;
+
+    @PostMapping("/create")
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.createStudent(student);
+    }
+}
