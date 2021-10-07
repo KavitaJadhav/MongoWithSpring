6dffdb2d7b9312ab2bccb24bd64befe2d1918636
Kavita Jadhav <jadhavkavita206@gmail.com>
2021-10-07 20:33:01 +0530

    Added an endpoint to get all students.

--- src/main/java/com/example/service/StudentService.java
+++ src/main/java/com/example/service/StudentService.java
@@ -5,6 +5,8 @@ import com.example.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudentService {
     @Autowired
@@ -17,5 +19,9 @@ public class StudentService {
 
     public Student getStudentById(String id) {
         return studentRepository.findById(id).get();
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
     }
 }
