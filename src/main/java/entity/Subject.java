838f5d11dee618a89fd61dec1403596abcdda0e5
Kavita Jadhav <jadhavkavita206@gmail.com>
2021-10-07 10:23:00 +0530

    Added base classes

---
+++ src/main/java/entity/Subject.java
@@ -0,0 +1,29 @@
+package entity;
+
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
+
+public class Subject {
+    @Field(name = "subject_name")
+    private String subjectName;
+
+    @Field(name = "mark_obtained")
+    private int markObtained;
+
+    public int getMarkObtained() {
+        return markObtained;
+    }
+
+    public void setMarkObtained(int markObtained) {
+        this.markObtained = markObtained;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+}
